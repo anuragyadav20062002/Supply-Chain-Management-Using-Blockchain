4b76ab33265c0a7b1a66583ea16f993f9c5206b3
Anurag Yadav <anuragyadav20062002@gmail.com>
2023-10-25 21:09:29 +0530

    added contracts

---
+++ contracts/Types.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.8;
+
+/**
+ * @title User defined data types
+ * @dev All the custom types that we have used in the supply chain
+ */
+library Types {
+    enum UserRole {
+        Manufacturer, // 0
+        Supplier, // 1
+        Vendor, // 2
+        Customer // 3
+    }
+
+    struct UserDetails {
+        UserRole role;
+        address id_;
+        string name;
+        string email;
+    }
+
+    enum ProductType {
+        BCG, // 0
+        RNA, // 1
+        MRNA, // 2
+        MMR, // 3
+        NasalFlu // 4
+    }
+
+    struct UserHistory {
+        address id_; // account Id of the user
+        uint256 date; // Added, Purchased date in epoch in UTC timezone
+    }
+
+    struct ProductHistory {
+        UserHistory manufacturer;
+        UserHistory supplier;
+        UserHistory vendor;
+        UserHistory[] customers;
+    }
+
+    struct Product {
+        string name;
+        string manufacturerName;
+        address manufacturer;
+        uint256 manDateEpoch;
+        uint256 expDateEpoch;
+        bool isInBatch; // few products will be packed & sold in batches
+        uint256 batchCount; // QTY that were packed in single batch
+        string barcodeId;
+        string productImage;
+        ProductType productType;
+        string scientificName;
+        string usage;
+        string[] composition;
+        string[] sideEffects;
+    }
+}
